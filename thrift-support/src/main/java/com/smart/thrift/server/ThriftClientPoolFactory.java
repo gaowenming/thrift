443237eccb9b732862f8f2a043359fc7701810c3
gaowenming <gaowm0207@163.com>
2015-10-15 10:05:01 +0800

    add more zkclient

--- thrift-support/src/main/java/com/smart/thrift/server/ThriftClientPoolFactory.java
+++ thrift-support/src/main/java/com/smart/thrift/server/ThriftClientPoolFactory.java
@@ -20,85 +20,84 @@ import com.smart.thrift.server.zookeeper.ThriftServerAddressProvider;
  */
 public class ThriftClientPoolFactory extends BasePoolableObjectFactory<TServiceClient> {
 
-	private Logger logger = LoggerFactory.getLogger(getClass());
-	
-	private final ThriftServerAddressProvider serverAddressProvider;
-	private final TServiceClientFactory<TServiceClient> clientFactory;
-	private PoolOperationCallBack callback;
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    
+    private final ThriftServerAddressProvider serverAddressProvider;
+    private final TServiceClientFactory<TServiceClient> clientFactory;
+    private PoolOperationCallBack callback;
 
-	protected ThriftClientPoolFactory(ThriftServerAddressProvider addressProvider, TServiceClientFactory<TServiceClient> clientFactory) throws Exception {
-		this.serverAddressProvider = addressProvider;
-		this.clientFactory = clientFactory;
-	}
+    protected ThriftClientPoolFactory(ThriftServerAddressProvider addressProvider, TServiceClientFactory<TServiceClient> clientFactory) throws Exception {
+        this.serverAddressProvider = addressProvider;
+        this.clientFactory = clientFactory;
+    }
 
-	protected ThriftClientPoolFactory(ThriftServerAddressProvider addressProvider, TServiceClientFactory<TServiceClient> clientFactory,
-			PoolOperationCallBack callback) throws Exception {
-		this.serverAddressProvider = addressProvider;
-		this.clientFactory = clientFactory;
-		this.callback = callback;
-	}
+    protected ThriftClientPoolFactory(ThriftServerAddressProvider addressProvider, TServiceClientFactory<TServiceClient> clientFactory,
+            PoolOperationCallBack callback) throws Exception {
+        this.serverAddressProvider = addressProvider;
+        this.clientFactory = clientFactory;
+        this.callback = callback;
+    }
 
-	static interface PoolOperationCallBack {
-		// 销毁client之前执行
-		void destroy(TServiceClient client);
+    static interface PoolOperationCallBack {
+        // 销毁client之前执行
+        void destroy(TServiceClient client);
 
-		// 创建成功是执行
-		void make(TServiceClient client);
-	}
+        // 创建成功是执行
+        void make(TServiceClient client);
+    }
 
-	@Override
-	public void destroyObject(TServiceClient client) throws Exception {
-		if (callback != null) {
-			try {
-				callback.destroy(client);
-			} catch (Exception e) {
-				logger.warn("destroyObject:{}", e);
-			}
-		}
-		logger.info("destroyObject:{}", client);
-		TTransport pin = client.getInputProtocol().getTransport();
-		pin.close();
-		TTransport pout = client.getOutputProtocol().getTransport();
-		pout.close();
-	}
+    @Override
+    public void destroyObject(TServiceClient client) throws Exception {
+        if (callback != null) {
+            try {
+                callback.destroy(client);
+            } catch (Exception e) {
+                logger.warn("destroyObject:{}", e);
+            }
+        }
+        logger.info("destroyObject:{}", client);
+        TTransport pin = client.getInputProtocol().getTransport();
+        pin.close();
+        TTransport pout = client.getOutputProtocol().getTransport();
+        pout.close();
+    }
 
-	@Override
-	public void activateObject(TServiceClient client) throws Exception {
-	}
+    @Override
+    public void activateObject(TServiceClient client) throws Exception {
+    }
 
-	@Override
-	public void passivateObject(TServiceClient client) throws Exception {
-	}
+    @Override
+    public void passivateObject(TServiceClient client) throws Exception {
+    }
 
-	@Override
-	public boolean validateObject(TServiceClient client) {
-		TTransport pin = client.getInputProtocol().getTransport();
-		logger.info("validateObject input:{}", pin.isOpen());
-		TTransport pout = client.getOutputProtocol().getTransport();
-		logger.info("validateObject output:{}", pout.isOpen());
-		return pin.isOpen() && pout.isOpen();
-	}
+    @Override
+    public boolean validateObject(TServiceClient client) {
+        TTransport pin = client.getInputProtocol().getTransport();
+        logger.info("validateObject input:{}", pin.isOpen());
+        TTransport pout = client.getOutputProtocol().getTransport();
+        logger.info("validateObject output:{}", pout.isOpen());
+        return pin.isOpen() && pout.isOpen();
+    }
 
-	@Override
-	public TServiceClient makeObject() throws Exception {
-		InetSocketAddress address = serverAddressProvider.selector();
-		if(address==null){
-			logger.warn("found no registered service...");
-			return null;
-		}
-		TSocket tsocket = new TSocket(address.getHostName(), address.getPort());
-		TTransport transport = new TFramedTransport(tsocket);
-		TProtocol protocol = new TBinaryProtocol(transport);
-		TServiceClient client = this.clientFactory.getClient(protocol);
-		transport.open();
-		if (callback != null) {
-			try {
-				callback.make(client);
-			} catch (Exception e) {
-				logger.warn("makeObject:{}", e);
-			}
-		}
-		return client;
-	}
+    @Override
+    public TServiceClient makeObject() throws Exception {
+        InetSocketAddress address = serverAddressProvider.selector();
+        if(address==null){
+            new ThriftException("No provider available for remote service");
+        }
+        TSocket tsocket = new TSocket(address.getHostName(), address.getPort());
+        TTransport transport = new TFramedTransport(tsocket);
+        TProtocol protocol = new TBinaryProtocol(transport);
+        TServiceClient client = this.clientFactory.getClient(protocol);
+        transport.open();
+        if (callback != null) {
+            try {
+                callback.make(client);
+            } catch (Exception e) {
+                logger.warn("makeObject:{}", e);
+            }
+        }
+        return client;
+    }
 
 }
